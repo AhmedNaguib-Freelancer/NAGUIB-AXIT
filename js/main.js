f59e94f28dc6c12c79af9b9c09bbc7beae9354dc
Ahmed Naguib <46289250+AhmedNaguib-Freelancer@users.noreply.github.com>
2018-12-31 18:39:34 -0800

    JS
    JS files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//tabs
+  $('.tab-switch li') .click(function) () {
+    $(this).addclass('selected')
+    .siblings().removeclass('selected')};
+    $('.'+ $(this).data('selected')).show();
+    window .console .log($(this).data('class')).hidden();
+});
+});
+
+
